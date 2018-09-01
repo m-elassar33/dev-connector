771086df817443d1259574f0174fbe97309ea1a8
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-09-01 16:51:13 +0200

    Add a key for JWT in keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://mostafa:mostafa1@ds123400.mlab.com:23400/devconnector'
+  mongoURI: 'mongodb://mostafa:mostafa1@ds123400.mlab.com:23400/devconnector',
+  secretOrKey: 'secret'
 };
