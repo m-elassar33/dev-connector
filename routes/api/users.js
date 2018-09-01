4ad95809a696869dc1cfade5769aa4e7c277564a
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-09-01 16:40:01 +0200

    Add the login logic

--- routes/api/users.js
+++ routes/api/users.js
@@ -46,4 +46,29 @@ router.post('/register', (req, res) => {
   });
 });
 
+// @route   POST api/users/login
+// @desc    Login User / Returning JWT Token
+// @access  Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
+
+    // Check Password
+    bycrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ msg: 'success' });
+      } else {
+        return res.status(400).json({ password: 'Password incorrect' });
+      }
+    });
+  });
+});
+
 module.exports = router;
