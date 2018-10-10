a6e02cb6acfe4fced399d8ae1a2006c3cdf78254
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-10-10 14:37:26 +0200

    Fix a bug in setCurrentUser

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -42,7 +42,7 @@ export const loginUser = userData => dispatch => {
 };
 
 // Set logged in user
-export const setCurrentUser = decoded => dispatch => {
+export const setCurrentUser = decoded => {
   return {
     type: SET_CURRENT_USER,
     payload: decoded
