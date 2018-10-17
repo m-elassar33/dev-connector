d9146d04fe8643ce5126e55f87663239dec06687
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-10-17 17:00:05 +0200

    Add content to ProfileGithub

--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -1,13 +1,74 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class ProfileGithub extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      clientId: '6d5f28eebcf38b064cc8',
+      clientSecret: '8ab959bf94b28ec27b64038c3f207e0a43659635',
+      count: 5,
+      sort: 'created: asc',
+      repos: []
+    };
+  }
+
+  componentDidMount() {
+    const { username } = this.props;
+    const { clientId, clientSecret, count, sort } = this.state;
+
+    fetch(
+      `https://api.github.com/users/${username}/repos?per_page=${count}&sort=${sort}&client_id=${clientId}&client_secret=${clientSecret}`
+    )
+      .then(res => res.json())
+      .then(data => {
+        if (this.refs.myRef) {
+          this.setState({ repos: data });
+        }
+      })
+      .catch(err => console.log(err));
+  }
+
   render() {
+    const { repos } = this.state;
+
+    const repoItems = repos.map(repo => (
+      <div key={repo.id} className="card card-body mb-2">
+        <div className="row">
+          <div className="col-md-6">
+            <h4>
+              <a href={repo.html_url} className="text-info" target="_blank">
+                {repo.name}
+              </a>
+            </h4>
+            <p>{repo.description}</p>
+          </div>
+          <div className="col-md-6">
+            <span className="bagde badge-info mr-1">
+              Stars: {repo.stargazers_count}
+            </span>
+            <span className="bagde badge-secondary mr-1">
+              Watchers: {repo.watchers_count}
+            </span>
+            <span className="bagde badge-success mr-1">
+              Forks: {repo.forks_count}
+            </span>
+          </div>
+        </div>
+      </div>
+    ));
     return (
-      <div>
-        <h1>TODO: PROFILE GITHUB</h1>
+      <div ref="myRef">
+        <hr />
+        <h3 className="mb-4">Latest Github Repos</h3>
+        {repoItems}
       </div>
     );
   }
 }
+
+ProfileGithub.propTypes = {
+  username: PropTypes.string.isRequired
+};
 
 export default ProfileGithub;
