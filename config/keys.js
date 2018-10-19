0aa9a1aaffd33aae6d5f7bb1a71dbc7c821e6fb1
Mostafa El-Assar <mostafa.elassarg15@gmail.com>
2018-10-19 02:09:50 +0200

    Secure the keys

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb://mostafa:mostafa1@ds123400.mlab.com:23400/devconnector',
-  secretOrKey: 'secret'
-};
+if (process.env.NODE_DEV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
